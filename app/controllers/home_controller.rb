0b9e254281bbfdba1dd7a1aed0afd3ea65c6c137
Maxime Lazzarotto <lazzarottomaxime@gmail.com>
2025-02-17 14:56:15 +0100

    création des routes et des views associés

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def parcours
+    # Action pour la page "Mon parcours"
+  end
+
+  def projets
+    # Action pour la page "Mes projets"
+  end
+
+  def contact
+    # Action pour la page "Me contacter"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-    root 'home#index'
+  root 'home#index'
+  get 'about', to: 'home#about'
+  get 'parcours', to: 'home#parcours'
+  get 'projets', to: 'home#projets'
+  get 'contacts', to: 'home#contacts'
 end
